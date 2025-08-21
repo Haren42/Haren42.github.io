01aff6bf1f8370e7d065d89be6e935d292f55247
Haren42 <harald.thoren@elevmail.stockholm.se>
2025-08-21 14:45:16 +0200

    Update quiz.py

--- programs/quiz.py
+++ programs/quiz.py
@@ -1,2 +1,15 @@
-namn = input("Vem är du?")
-print(f"Hej, {namn}")
+import random
+namn = input("Who are you?")
+namn.strip().lower()
+print(f"Hej, {namn}!)
+antalfragor = input("How many questions do you want?")
+for _ in range(antalfragor):
+  villkenfraga = random.randint(1, 3)
+    if vilkenfraga == 1:
+      svar=input("Who wrote the book where 42 is the answer to the ultimate qustion?")
+        svar.strip().lower()
+        if svar == "douglas adams":
+          print("Great!")
+        else: 
+        print("Wrong, the right answer is Douglas adams.)
+  
